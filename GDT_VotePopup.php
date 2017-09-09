854320b2534b4e06f92016e6acb26f3cdc3feaa4
gizmore <gizmore@wechall.net>
2017-09-09 02:39:37 +0200

    votepopup_tryout

---
+++ GDT_VotePopup.php
@@ -0,0 +1,23 @@
+<?php
+namespace GDO\Vote;
+use GDO\UI\GDT_IconButton;
+use GDO\Template\GDT_Template;
+/**
+ * Show a trophy with level badge.
+ * A tooltip explains if your access is granted or restricted.
+ * @author gizmore
+ */
+final class GDT_VotePopup extends GDT_IconButton
+{
+    public $level = 0;
+    public function level(int $level)
+    {
+        $this->level = $level;
+        return $this;
+    }
+    
+    public function renderCell()
+    {
+        return GDT_Template::php('Vote', 'cell/vote_popup.php', ['field'=>$this]);
+    }
+}
