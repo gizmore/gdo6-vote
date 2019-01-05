a2fbd830a15d994ef15c85e0ce58aaa31ca0002d
gizmore <gizmore@wechall.net>
2019-01-05 14:07:30 +0100

    6.09-r7_multiple_likes_per_user

--- GDO_LikeTable.php
+++ GDO_LikeTable.php
@@ -4,8 +4,10 @@ namespace GDO\Vote;
 use GDO\Core\GDO;
 use GDO\DB\GDT_Object;
 use GDO\Net\GDT_IP;
-use GDO\User\GDT_User;
 use GDO\User\GDO_User;
+use GDO\User\GDT_User;
+use GDO\DB\GDT_CreatedAt;
+use GDO\DB\GDT_AutoInc;
 
 class GDO_LikeTable extends GDO
 {
@@ -14,15 +16,19 @@ class GDO_LikeTable extends GDO
 	 */
 	public function gdoLikeObjectTable() {}
 	public function gdoLikeForGuests() { return true; }
+	public function gdoMaxLikeCount() { return 1; }
+	public function gdoLikeCooldown() { return 60*60*24; }
 	
 	public function gdoCached() { return false; }
 	public function gdoAbstract() { return $this->gdoLikeObjectTable() === null; }
 	public function gdoColumns()
 	{
 		return array(
-			GDT_User::make('like_user')->primary(),
-			GDT_Object::make('like_object')->table($this->gdoLikeObjectTable())->primary(),
+			GDT_AutoInc::make('like_id'),
+			GDT_User::make('like_user'),
+			GDT_Object::make('like_object')->table($this->gdoLikeObjectTable()),
 			GDT_IP::make('like_ip')->notNull(),
+			GDT_CreatedAt::make('like_created'),
 		);
 	}
 	
--- Method/Like.php
+++ Method/Like.php
@@ -9,6 +9,7 @@ use GDO\User\GDO_User;
 use GDO\Util\Common;
 use GDO\Vote\GDO_LikeTable;
 use GDO\Vote\GDT_LikeButton;
+use GDO\Date\Time;
 
 /**
  * The method to like an item.
@@ -44,27 +45,42 @@ final class Like extends Method
 		# Get GDO row, e.g. Link
 		$object = $objects->find(Common::getRequestString('id'));
 		
+		# Check IP count
 		$count = $table->countWhere(sprintf("like_object=%s AND like_ip='%s'", $object->getID(), GDT_IP::current()));
-		
-		if ($count === 0)
+		if ($count >= $table->gdoMaxLikeCount())
 		{
-			# Vote
-			$like = $class::blank(array(
-				'like_user' => $user->getID(),
-				'like_object' => $object->getID(),
-				'like_ip' => GDT_IP::current(),
-			));
-			$like instanceof GDO_LikeTable;
-			$like->replace();
-			
-			# Update cache
-			$object->updateLikes();
-
-			return GDT_Response::makeWith(
-				GDT_LikeButton::make('likes')->gdo($object)
-			);
+			return $this->error('err_vote_ip');
 		}
 		
-		return $this->error('err_vote_ip');
+		# Check user count
+		$count = $table->countWhere(sprintf("like_object=%s AND like_user='%s'", $object->getID(), $user->getID()));
+		if ($count >= $table->gdoMaxLikeCount())
+		{
+			return $this->error('err_vote_count');
+		}
+		
+		# Check last vote date against cooldown
+		$lastVoteDate = $table->select('like_created')->where(sprintf("like_object=%s AND (like_user=%s or like_ip='%s')", $object->getID(), $user->getID(), GDT_IP::current()))->exec()->fetchValue();
+		if ( $lastVoteDate && (Time::getAgo($lastVoteDate) < $table->gdoLikeCooldown()) )
+		{
+			return $this->error('err_vote_frequency', [Time::humanDuration($table->gdoLikeCooldown())]);
+		}
+		
+		# Vote
+		$like = $class::blank(array(
+			'like_user' => $user->getID(),
+			'like_object' => $object->getID(),
+			'like_ip' => GDT_IP::current(),
+		));
+		$like instanceof GDO_LikeTable;
+		$like->insert();
+
+		# Update cache
+		$object->updateLikes();
+
+		return GDT_Response::makeWith(
+			GDT_LikeButton::make('likes')->gdo($object)
+		);
+		
 	}
 }
