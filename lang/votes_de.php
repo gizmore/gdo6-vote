7199eb5140ee0f50d8595f7b463955089f67f5c8
gizmore <gizmore@wechall.net>
2019-08-31 16:29:12 +0200

    6.10-r42_german_lang

---
+++ lang/votes_de.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+'cfg_likes' => 'Anzahl der Likes',
+##########################################################
+'err_vote_ip' => 'Ihre IP hat kürzlich hierfür gestimmt.',
+'err_vote_guest' => 'Gäste dürfen hierfür keine Stimme abgeben.',
+'votes' => 'Bewertungen',
+'rating' => 'Bewertung',
+'tt_gdo_vote_open' => 'Es gab bislang nur %s/%s Stimmen.',
+##########################################################
+'err_vote_count' => 'Sie haben hierfür mehr als %s mal abgestimmt.',
+'err_vote_frequency' => 'Sie haben kürzlich zu viele Stimmen abgegen: %s.',
+##########################################################
+'dlg_votepopup_title' => 'Bewerten Sie dies',
+'votepopup_votes' => 'Stimmen: %s',
+'votepopup_rating' => 'Bewertung: %s',
+####
+'btn_likes' => 'Likes',
+);
