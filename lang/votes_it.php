46bcdf06ed4bc1f37da96fa5e8e7ea902d46af91
gizmore <gizmore@wechall.net>
2019-12-22 18:59:43 +0100

    6.10-r1042_it

---
+++ lang/votes_it.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+'cfg_likes' => 'Quantità di link',
+##########################################################
+'err_vote_ip' => 'La vostra IP ha votato ultimamente.',
+'err_vote_guest' => 'Ospiti non possono dare il suo voto.',
+'votes' => 'Valutazioni',
+'rating' => 'Valutazione',
+'tt_gdo_vote_open' => 'Ci sono stati solo %s/%s voti.',
+##########################################################
+'err_vote_count' => 'Avete votato piu di %s volte.',
+'err_vote_frequency' => 'Avete consegnato troppi voti: %s.',
+##########################################################
+'dlg_votepopup_title' => 'Valuti questo:',
+'votepopup_votes' => 'Voto: %s',
+'votepopup_rating' => 'Valutazione: %s',
+####
+'btn_likes' => 'Likes',															
+);
