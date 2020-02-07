9f9b927c62c317a308eeeaefda9c37e585ad23a5
gizmore <gizmore@wechall.net>
2020-02-07 19:01:43 +0100

    6.10-r5023_link_relation

--- tpl/cell/vote_selection.php
+++ tpl/cell/vote_selection.php
@@ -5,5 +5,5 @@ $own = $field->ownVote();
 $max = $vt->gdoVoteMax(); ?>
 <?php for ($i = 1; $i <= $max; $i++) : ?>
 <?php $color = $own < $i ? '#999' : '#ffd700'; ?>
-<?= GDT_IconButton::make()->icon('star')->color($color)->href($field->hrefVoteScore($i))->render(); ?>
+<?= GDT_IconButton::make()->icon('star')->noFollow()->color($color)->href($field->hrefVoteScore($i))->render(); ?>
 <?php endfor; ?>
