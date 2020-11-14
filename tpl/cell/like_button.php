4477ee13ae99adea51e383c292c7cb69fc8784ac
gizmore <gizmore@wechall.net>
2020-11-14 18:01:44 +0100

    6.10-r9378_tables

--- tpl/cell/like_button.php
+++ tpl/cell/like_button.php
@@ -6,4 +6,4 @@ $user = GDO_User::current();
 $gdo = $field->getLikeObject();
 $liked = $gdo->hasLiked($user);
 $likes = GDT_LikeCount::make()->gdo($gdo)->render();
-echo GDT_Link::make()->klass('gdt-like-button')->icon('like')->href($field->href)->editable($field->editable)->rawLabel($likes)->render();
+echo GDT_Link::make()->klass('gdt-like-button')->icon('like')->href($field->href)->editable($field->editable)->labelRaw($likes)->render();
